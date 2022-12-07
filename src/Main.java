bbbbafcd3fe080a4f28cce1354ef3b3d8446a034
Vitalina Volkivska <113260060+Vitalina-V@users.noreply.github.com>
2022-12-07 17:40:06 +0200

    second commit :wq

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My progect");
+        Klient klient = new Klient("Anna", "Petrenko", 28);
+        System.out.println("New klient is " + klient.getName()+ " " + klient.getSurName());
     }
 }
